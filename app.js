b4f7e08689c95f3758edcffc6063aad36c41d273
darlyo <mainguene.kevin@gmail.com>
2017-03-21 11:26:09 +0100

    Graphisme
    mdification du nom du lien de la page admin
    detection probleme lors d'appuie sur les boutton sur le telephone

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var net = require('net');
 
 // Variables reseau Can
-var HOST = '192.168.173.9';
+var HOST = '192.168.173.149';
 var PORT = 30000;
 
 // Variables de connection
